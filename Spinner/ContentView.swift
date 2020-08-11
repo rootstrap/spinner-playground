e22f2884b592e79cf7357c06248656e5ae7a7e71
Pablo Malvasio <p.a.malvasio@gmail.com>
2020-08-11 16:33:32 -0300

    Spinner View

--- Spinner/ContentView.swift
+++ Spinner/ContentView.swift
@@ -9,13 +9,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  @State var showSpinner = false
+  
+  var body: some View {
+    ZStack {
+      ScrollView {
+        VStack(alignment: .leading, spacing: 20) {
+          HStack {
+            Text("List")
+              .font(.system(size: 30, weight: .bold))
+            
+            Spacer()
+            
+            Button(action: { self.showSpinner.toggle() }) {
+              Image(systemName: "arrow.2.circlepath")
+                .font(.system(size: 25, weight: .medium))
+                .foregroundColor(.black)
+                .frame(width: 44, height: 44)
+            }
+          }
+          
+          CellView()
+          CellView()
+          CellView()
+        }
+        .padding()
+      }
+      
+      if showSpinner {
+        SpinnerView()
+          .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
+              withAnimation(.easeIn) {
+                self.showSpinner.toggle()
+              }
+            }
+          }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+struct CellView: View {
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading, spacing: 8) {
+        Text("Lorem ipsum")
+          .font(.title)
+          .bold()
+        Text("Accumsan velit orci fermentum, etiam nonummy orci pellentesque sed maecenas consectetuer, hymenaeos cras ac.")
+          .font(.subheadline)
+          .foregroundColor(.secondary)
+        
+        LinearGradient(
+          gradient: Gradient(colors: [Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)), Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))]),
+          startPoint: .topLeading,
+          endPoint: .bottomTrailing
+        )
+        .cornerRadius(20)
+      }
+      .padding()
     }
+      
+    .frame(height: 250)
+    .background(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)).opacity(0.2))
+    .cornerRadius(20)
+  }
 }
---
+++ Spinner/SpinnerView.swift
@@ -0,0 +1,73 @@
+//
+//  SpinnerView.swift
+//  Spinner
+//
+//  Created by Pablo Malvasio on 8/11/20.
+//  Copyright © 2020 Rootstrap. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpinnerConfiguration {
+  var spinnerColor: Color = .white
+  var blurredBackground: Color = .black
+  var spinnerBackgroundColor: Color = .gray
+  var backgroundCornerRadius: CGFloat = 30
+  var width: CGFloat = 50
+  var height: CGFloat = 50
+  var speed: Double = 1
+}
+
+struct SpinnerView: View {
+  var configuration: SpinnerConfiguration = SpinnerConfiguration()
+  @State var isAnimating = false
+  
+  var body: some View {
+    let multiplier = configuration.width / 40
+    
+    return
+      ZStack {
+        configuration.blurredBackground.opacity(0.8)
+          .edgesIgnoringSafeArea(.all)
+          .blur(radius: 200)
+        
+        ZStack {
+          configuration.spinnerBackgroundColor.opacity(0.5)
+          
+          Circle()
+            .trim(from: 0.2, to: 1)
+            .stroke(
+              Color.white,
+              style: StrokeStyle(
+                lineWidth: 5 * multiplier,
+                lineCap: .round,
+                lineJoin: .round,
+                dash: [20, 0],
+                dashPhase: 0
+              )
+            )
+            .frame(width: configuration.width, height: configuration.height)
+            .shadow(color: Color.black.opacity(0.1), radius: 3, x: 0, y: 3)
+            .rotationEffect(.degrees(isAnimating ? 360 : 0))
+            .animation(
+              Animation.linear(duration: configuration.speed)
+                .repeatForever(autoreverses: false)
+            )
+        }
+        .frame(width: 80 * multiplier, height: 80 * multiplier)
+        .background(Color.white)
+        .cornerRadius(configuration.backgroundCornerRadius)
+        .shadow(color: Color.white.opacity(0.3), radius: 5, x: 0, y: 5)
+        .shadow(color: Color.black.opacity(0.1), radius: 2, x: 0, y: 2)
+        .onAppear {
+          self.isAnimating = true
+        }
+    }
+  }
+}
+
+struct SpinnerView_Previews: PreviewProvider {
+  static var previews: some View {
+    SpinnerView()
+  }
+}
