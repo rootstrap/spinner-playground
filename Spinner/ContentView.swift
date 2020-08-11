8352032e45983f612d7e262e879aff0de228cf36
Pablo Malvasio <p.a.malvasio@gmail.com>
2020-08-11 15:44:54 -0300

    Initial Commit

---
+++ Spinner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spinner
+//
+//  Created by Pablo on 8/11/20.
+//  Copyright © 2020 PaperJam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
